c99ebe1426b091b76b2f46c765eacec03d395509
DiegoLentz <diegoolentz@gmail.com>
2022-07-16 14:43:53 -0300

    practica_semama5_CollectionCustom

---
+++ web-practicaSemanal/src/main/java/practica_semana5_CollectionCustom/T.java
@@ -0,0 +1,17 @@
+
+package practica_semana5_CollectionCustom;
+
+public class T {
+	private String nombre;
+
+	public T(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+	
+	
+
+}
